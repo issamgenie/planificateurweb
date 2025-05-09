3fc15360a1db5e77d1ba4542ffb60c6c28f1dc52
issamgenie <issam.genie@gmail.com>
2025-05-09 09:11:31 -0400

    Changer le defaut de l'appel au variable d environnement

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Lire l'URL de l'API depuis les variables d'environnement de Vite
-const VITE_API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+const VITE_API_URL = import.meta.env.VITE_API_URL || 'https://app-c73674a0-3a96-44d9-a4e1-5eab37266b1f.cleverapps.io/api';
 
 const apiClient = axios.create({
   baseURL: VITE_API_URL,
